16932bf07e358a25c7783b6d8946b9a1c5488a1b
cpojer <christoph.pojer@gmail.com>
2025-04-24 18:13:12 +0900

    Initial Commit.

---
+++ vitest.config.js
@@ -0,0 +1,17 @@
+// this is needed for react jsx support
+import fbteePreset from '@nkzw/babel-preset-fbtee';
+import react from '@vitejs/plugin-react';
+import reactNative from 'vitest-react-native';
+
+export default {
+  plugins: [
+    reactNative(),
+    react(
+      react({
+        babel: {
+          presets: [fbteePreset],
+        },
+      }),
+    ),
+  ],
+};

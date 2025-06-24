905b38e646777c627ad1bfc0ff0d92428c7ae0e8
cpojer <christoph.pojer@gmail.com>
2025-06-24 10:02:46 +0900

    Use prettier 3.6.0.

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,7 @@
 export default {
   importOrderParserPlugins: ['importAssertions', 'typescript', 'jsx'],
   plugins: [
+    '@prettier/plugin-oxc',
     '@ianvs/prettier-plugin-sort-imports',
     'prettier-plugin-packagejson',
     // The order of plugins matters, and Tailwind CSS must be the last one.

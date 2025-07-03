bbe895748f62685e5a501ee9f60f6e4b6cd1acd7
cpojer <christoph.pojer@gmail.com>
2025-07-03 12:30:00 +0900

    Update deps.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,7 @@
 import 'global.css';
 import { getLocales } from 'expo-localization';
 import { Slot } from 'expo-router';
-import { LocaleContext } from 'fbtee';
+import { createLocaleContext } from 'fbtee';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { ViewerContext } from 'src/user/useViewerContext.tsx';
 import ja_JP from '../translations/ja_JP.json' with { type: 'json' };
@@ -11,26 +11,23 @@ export const unstable_settings = {
   initialRouteName: '(app)',
 };
 
-const availableLanguages = new Map([
-  ['en_US', 'English'],
-  ['ja_JP', '日本語 (Japanese)'],
-] as const);
-
-const clientLocales = getLocales().map(({ languageTag }) => languageTag);
-const loadLocale = async (locale: string) => {
-  if (locale === 'ja_JP') {
-    return ja_JP.ja_JP;
-  }
-  return {};
-};
+const LocaleContext = createLocaleContext({
+  availableLanguages: new Map([
+    ['en_US', 'English'],
+    ['ja_JP', '日本語 (Japanese)'],
+  ] as const),
+  clientLocales: getLocales().map(({ languageTag }) => languageTag),
+  loadLocale: async (locale: string) => {
+    if (locale === 'ja_JP') {
+      return ja_JP.ja_JP;
+    }
+    return {};
+  },
+});
 
 export default function RootLayout() {
   return (
-    <LocaleContext
-      availableLanguages={availableLanguages}
-      clientLocales={clientLocales}
-      loadLocale={loadLocale}
-    >
+    <LocaleContext>
       <ViewerContext>
         <GestureHandlerRootView>
           <VStack flex1>

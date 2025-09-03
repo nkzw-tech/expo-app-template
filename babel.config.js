486742dd54a0ca0e87ce507f14b8acb744ff806f
cpojer <christoph.pojer@gmail.com>
2025-09-03 09:28:25 +0200

    Add Relay.

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,16 @@ export default function (api) {
   api.cache(true);
 
   return {
+<<<<<<< HEAD
     plugins: [
       ['@babel/plugin-transform-private-methods', { loose: true }],
       ['@babel/plugin-transform-class-properties', { loose: true }],
       ['@babel/plugin-transform-private-property-in-object', { loose: true }],
     ],
+||||||| parent of 2d8b304 (Add Relay.)
+=======
+    plugins: ['babel-plugin-relay'],
+>>>>>>> 2d8b304 (Add Relay.)
     presets: [
       '@nkzw/babel-preset-fbtee',
       [

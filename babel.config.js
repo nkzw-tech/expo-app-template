6a687197965ac5a7c2f46097acbceeb03d2d782f
cpojer <christoph.pojer@gmail.com>
2025-10-07 14:11:19 +0900

    Fix Expo config with decorators.

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,17 @@ export default function (api) {
   api.cache(true);
 
   return {
+    plugins: [
+      ['@babel/plugin-transform-private-methods', { loose: true }],
+      ['@babel/plugin-transform-class-properties', { loose: true }],
+      ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+    ],
     presets: [
       '@nkzw/babel-preset-fbtee',
-      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+      [
+        'babel-preset-expo',
+        { decorators: { version: '2023-11' }, jsxImportSource: 'nativewind' },
+      ],
       'nativewind/babel',
     ],
   };

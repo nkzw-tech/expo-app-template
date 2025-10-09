a2d9b2992f501fdcc05ee10a90f2fad1c6e641fd
cpojer <christoph.pojer@gmail.com>
2025-10-09 14:16:01 +0900

    Update deps.

--- babel.config.js
+++ babel.config.js
@@ -2,17 +2,9 @@ export default function (api) {
   api.cache(true);
 
   return {
-    plugins: [
-      ['@babel/plugin-transform-private-methods', { loose: true }],
-      ['@babel/plugin-transform-class-properties', { loose: true }],
-      ['@babel/plugin-transform-private-property-in-object', { loose: true }],
-    ],
     presets: [
       '@nkzw/babel-preset-fbtee',
-      [
-        'babel-preset-expo',
-        { decorators: { version: '2023-11' }, jsxImportSource: 'nativewind' },
-      ],
+      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
       'nativewind/babel',
     ],
   };
